b5a9db8d1d2d4548f8911e6e0f0ac4e2a129e342
sorenblank <afrozaman12@gmail.com>
2024-10-08 01:36:02 +0600

    feat: Completed 'Header' section

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,3 +1,40 @@
-export const Navbar = () => {
-  return null;
+import ArrowRight from "@/assets/arrow-right.svg";
+import Logo from "@/assets/logosaas.png";
+import MenuIcon from "@/assets/menu.svg";
+import Image from "next/image";
+
+export const Header = () => {
+  return (
+    <header className="sticky top-0 backdrop-blur-sm z-20">
+      <div className="flex justify-center items-center py-3 bg-black text-white text-sm gap-3">
+        <p className="text-white/60 hidden md:block">
+          Streamline your workflow and boost your productivity
+        </p>
+        <div className="inline-flex gap-1 items-center">
+          <p>Get Started for free</p>
+          <ArrowRight className="h-4 w-4 inline-flex justify-center items-center" />
+        </div>
+      </div>
+
+      <div className="py-5">
+        <div className="container">
+          <div className="flex items-center justify-between">
+            <Image src={Logo} alt="Motion Logo" height={40} width={40} />
+            <MenuIcon className="h-5 w-5 md:hidden" />
+
+            <nav className="hidden md:flex gap-6 text-black/60 items-center">
+              <a href="#">About</a>
+              <a href="#">Features</a>
+              <a href="#">Customers</a>
+              <a href="#">Updates</a>
+              <a href="#">Help</a>
+              <button className="bg-black text-white px-4 py-2 rounded-lg font-medium inline-flex items-center justify-center tracking-tight">
+                Get for Free
+              </button>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
