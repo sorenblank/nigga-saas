839f01524c6f1331291fc79f39709ba40a481f98
sorenblank <afrozaman12@gmail.com>
2024-10-08 03:46:00 +0600

    feat: Created `Pricing` section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { Header } from "@/components/sections/Header";
 import { Hero } from "@/components/sections/Hero";
 import { LogoTicker } from "@/components/sections/LogoTicker";
 import { ProductShowcase } from "@/components/sections/ProductShowcase";
+import { Pricing } from "@/components/sections/Pricing";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <Hero />
       <LogoTicker />
       <ProductShowcase />
+      <Pricing />
     </>
   );
 }
