6be239c12a57b2db539877aa724f95077505ecb8
sorenblank <afrozaman12@gmail.com>
2024-10-08 02:07:46 +0600

    Feat: Added 'LogoTicket'

--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,3 +1,27 @@
+import AcmeLogo from "@/assets/logo-acme.png";
+import QuantumLogo from "@/assets/logo-quantum.png";
+import EchoLogo from "@/assets/logo-echo.png";
+import CelestialLogo from "@/assets/logo-celestial.png";
+import PulseLogo from "@/assets/logo-pulse.png";
+import ApexLogo from "@/assets/logo-apex.png";
+
+import Image from "next/image";
+
 export const LogoTicker = () => {
-  return null;
+  return (
+    <div className="py-8 md:py-12 bg-white">
+      <div className="container">
+        <div className="flex overflow-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)]">
+          <div className="flex gap-14 flex-none">
+            <Image src={AcmeLogo} alt="AcmeLogo" className="h-8 w-auto" />
+            <Image src={QuantumLogo} alt="QuantumLogo" className="h-8 w-auto" />
+            <Image src={EchoLogo} alt="EchoLogo" className="h-8 w-auto" />
+            <Image src={CelestialLogo} alt="CelestialLogo" className="h-8 w-auto" />
+            <Image src={PulseLogo} alt="PulseLogo" className="h-8 w-auto" />
+            <Image src={ApexLogo} alt="ApexLogo" className="h-8 w-auto" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
