bab02ecee51afa654de00d4b6e3b977643ec5e2c
sorenblank <afrozaman12@gmail.com>
2024-10-08 04:19:38 +0600

    refactor: Updated utility class to make reusable

--- src/components/sections/ProductShowcase.tsx
+++ src/components/sections/ProductShowcase.tsx
@@ -8,25 +8,25 @@ export const ProductShowcase = () => {
   return (
     <section className="bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF] py-24 overflow-x-clip">
       <div className="container">
-        <div className="max-w-[540px] mx-auto">
+        <div className="section-heading">
           <div className="flex justify-center">
             <div className="text-sm inline-flex border border-[#222]/10 px-3 py-1 rounded-lg tracking-tight">
               Boost your producvitiy
             </div>
           </div>
-          <h2 className="text-center text-3xl md:text-[54px] md:leading-[60px] font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-5">
+          <h2 className="section-title mt-5">
             A more effective way to track your progress
           </h2>
 
-          <p className="text-center text-[22px] leading-[30px] tracking-tight text-[#010D3E] mt-5">
+          <p className="section-description mt-5">
             Celebrate the joy of accomplishment with an app designed to track your progress and motivate your efforts.
           </p>
         </div>
         <div className="relative">
-          <Image src={ ProductImage } alt="Product Image" className="mt-10" />
+          <Image src={ProductImage} alt="Product Image" className="mt-10" />
 
-          <Image src={ PyramidImage } alt="Pyramid Image" width={262} height={262} className="hidden md:block absolute -right-36 -top-32"/>
-          <Image src={ TubeImage } alt="Tube image" height={248} className="hidden md:block absolute bottom-24 -left-36"/>
+          <Image src={PyramidImage} alt="Pyramid Image" width={262} height={262} className="hidden md:block absolute -right-36 -top-32" />
+          <Image src={TubeImage} alt="Tube image" height={248} className="hidden md:block absolute bottom-24 -left-36" />
         </div>
       </div>
     </section>
