0a9d6283dab60a87707267e264ff1981ddfcb1a6
sorenblank <afrozaman12@gmail.com>
2024-10-10 05:26:00 +0600

    feat: Added parallax effect to `CTA` section

--- src/components/sections/CallToAction.tsx
+++ src/components/sections/CallToAction.tsx
@@ -1,11 +1,22 @@
+"use client";
+
 import ArrowRight from '@/assets/arrow-right.svg';
 import StarImage from '@/assets/star.png';
 import SpringImage from '@/assets/spring.png';
 import Image from 'next/image';
+import { motion, useScroll, useTransform } from 'framer-motion';
+import { useRef } from 'react';
 
 export const CallToAction = () => {
+  const sectionRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: sectionRef,
+    offset: ["start end", "end start"]
+  });
+
+  const TranslateY = useTransform(scrollYProgress, [0, 1], [150, -150]);
   return (
-    <section className="bg-gradient-to-b from-white to-[#D2DCFF] py-24 overflow-x-clip">
+    <section ref={sectionRef} className="bg-gradient-to-b from-white to-[#D2DCFF] py-24 overflow-x-clip">
       <div className="container">
         <div className='section-heading relative'>
           <h2 className="section-title">
@@ -16,8 +27,8 @@ export const CallToAction = () => {
             Celebrate the joy of accomplishment with an app designed to track your progress and motivate your efforts.
           </p>
 
-          <Image src={StarImage} alt="Star Image" width={360} className='absolute -left-[350px] -top-[137px]' />
-          <Image src={SpringImage} alt="Spring Image" width={360} className='absolute -right-[331px] -top-[19px]' />
+          <motion.img src={StarImage.src} alt="Star Image" width={360} className='absolute -left-[350px] -top-[137px]' style={{ translateY: TranslateY }} />
+          <motion.img src={SpringImage.src} alt="Spring Image" width={360} className='absolute -right-[331px] -top-[19px]' style={{ translateY: TranslateY }} />
         </div>
 
         <div className="flex gap-2 mt-10 justify-center">
