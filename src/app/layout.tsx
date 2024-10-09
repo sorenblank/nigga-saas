4a6bd6345621c636c627976c8a1852712627d765
sorenblank <afrozaman12@gmail.com>
2024-10-10 05:51:58 +0600

    feat: Added MetaData

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { DM_Sans } from "next/font/google";
 import "./globals.css";
 import clsx from "clsx";
@@ -6,9 +6,41 @@ import clsx from "clsx";
 const dmSans = DM_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Light Saas Landing Page",
-  description: "Template created by Frontend Tribe",
+  metadataBase: new URL("https://niggsaas.vercel.app"),
+  title: "Nigga Saas Landing Page",
+  description: "A modern, responsive SaaS landing page built with Next.js, Tailwind CSS, and Framer Motion. This project showcases a sleek design with smooth animations and a mobile-first approach.",
+  openGraph: {
+    title: "Nigga Saas Landing Page",
+    description: "A modern, responsive SaaS landing page built with Next.js, Tailwind CSS, and Framer Motion. This project showcases a sleek design with smooth animations and a mobile-first approach.",
+    type: "website",
+    locale: "en_US",
+    url: new URL("/", "https://niggsaas.vercel.app"),
+    images: [
+      {
+        url: "https://niggsaas.vercel.app/og-image.png",
+        width: 1784,
+        height: 892,
+        alt: "niggsaas"
+      }
+    ]
+  },
+  twitter: {
+    creator: "@sorenblank",
+    site: "@sorenblank",
+    card: "summary_large_image",
+    images: [
+      {
+        url: "https://niggsaas.app/og-image.png",
+        alt: "niggsaas"
+      }
+    ]
+  },
 };
+
+// Viewport configuration
+export const viewport: Viewport = {
+  themeColor: '#93A5E5', // Theme color for the website
+}
 
 export default function RootLayout({
   children,
