3504547aeb338212dea2be3ae22d4c13f32adb94
sorenblank <afrozaman12@gmail.com>
2024-10-10 03:50:53 +0600

    feat: Created `Testimonials` section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { Hero } from "@/components/sections/Hero";
 import { LogoTicker } from "@/components/sections/LogoTicker";
 import { ProductShowcase } from "@/components/sections/ProductShowcase";
 import { Pricing } from "@/components/sections/Pricing";
+import { Testimonials } from "@/components/sections/Testimonials";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <LogoTicker />
       <ProductShowcase />
       <Pricing />
+      <Testimonials />
     </>
   );
 }
